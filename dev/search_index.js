d75b7f9dbe917acf55df15f00a38021669aa8ef8
Documenter.jl <documenter@juliadocs.github.io>
2022-06-14 09:01:49 +0000

    build based on fa8fe94

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Peridynamics","category":"page"},{"location":"#Peridynamics","page":"Home","title":"Peridynamics","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Peridynamics.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Peridynamics]","category":"page"}]
+}
