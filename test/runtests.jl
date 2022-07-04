fed59523e06f8d607e8cff1ee069ec31ebdbfa49
kfrb <kai.friebi@gmail.com>
2022-07-04 17:12:11 +0200

    Copy from private repo

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 using SafeTestsets
 
 @testset "Peridynamics.jl" begin
+    @safetestset "Types" begin include("types.jl") end
     @safetestset "Bonds" begin include("bonds.jl") end
     @safetestset "Symmetry" begin include("symmetry.jl") end
 end
