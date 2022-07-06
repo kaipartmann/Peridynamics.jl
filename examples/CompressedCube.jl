a0f2050ce415891adf31e6ac941368e4c525a974
kfrb <kai.friebi@gmail.com>
2022-07-06 16:30:22 +0200

    Formatting blue style and minor changes

--- examples/CompressedCube.jl
+++ examples/CompressedCube.jl
@@ -1,15 +1,16 @@
 using Peridynamics
-# read abaqus mesh and convert to point cloud
 pointcloud = read_inp("test/models/CubeC3D8.inp")
 material = BondBasedMaterial(; horizon=15, rho=7850, E=210e9, Gc=1000.0)
-# abaqus element-sets --> point-sets
 boundary_conditions = [
-    ForceDensityBC(x -> -1e5, pointcloud.point_sets["l"], 1),
-    ForceDensityBC(x -> 1e5, pointcloud.point_sets["r"], 1),
+    ForceDensityBC(t -> -1e5, pointcloud.point_sets["l"], 1),
+    ForceDensityBC(t -> 1e5, pointcloud.point_sets["r"], 1),
 ]
-job = PDSingleBodyAnalysis(; name="CompressedCube",
-    pc=pointcloud, mat=material, bcs=boundary_conditions,
-    td=TimeDiscretization(500, alg = :dynrelax),
+job = PDSingleBodyAnalysis(;
+    name="CompressedCube",
+    pc=pointcloud,
+    mat=material,
+    bcs=boundary_conditions,
+    td=TimeDiscretization(500; alg=:dynrelax),
     es=ExportSettings("examples/results/CompressedCube", 10),
 )
 submit(job)
--- examples/CrackedPlateUnderTension.jl
+++ examples/CrackedPlateUnderTension.jl
@@ -9,28 +9,34 @@ using Peridynamics
 length_x = 0.05 # [m]
 length_y = 0.05 # [m]
 length_z = 0.005 # [m]
-Δx = length_y / 60 # point spacing: 60 points along y-describe_interactions
+Δx = length_y / 60 # point spacing
 pc = PointCloud(length_x, length_y, length_z, Δx)
 
 ##--   3. DEFINE THE MATERIAL   ------------------------------------------------------------
 mat = BondBasedMaterial(;
-    horizon = 3.015Δx, # [m]
-    rho = 7850.0, # [kg/m^3]
-    E = 210e9, # [N/m^2]
-    Gc = 1000.0, # [N/m]
+    horizon=3.015Δx, # [m]
+    rho=7850.0, # [kg/m^3]
+    E=210e9, # [N/m^2]
+    Gc=1000.0, # [N/m]
 )
 
 ##--   3. PREDEFINED CRACKS   --------------------------------------------------------------
 cracklength = 0.5 * length_x
-precrack_set_a = findall((pc.position[2, :] .>= 0) .& (pc.position[2, :] .< 12 * Δx) .&
-                         (pc.position[1, :] .<= -length_x / 2 + cracklength))
-precrack_set_b = findall((pc.position[2, :] .<= 0) .& (pc.position[2, :] .> -12 * Δx) .&
-                         (pc.position[1, :] .<= -length_x / 2 + cracklength))
+precrack_set_a = findall(
+    (pc.position[2, :] .>= 0) .&
+    (pc.position[2, :] .< 12 * Δx) .&
+    (pc.position[1, :] .<= -length_x / 2 + cracklength),
+)
+precrack_set_b = findall(
+    (pc.position[2, :] .<= 0) .&
+    (pc.position[2, :] .> -12 * Δx) .&
+    (pc.position[1, :] .<= -length_x / 2 + cracklength),
+)
 precracks = [PreCrack(precrack_set_a, precrack_set_b)]
 
 ##--   4. DEFINE INITIAL AND BOUNDARY CONDITIONS   -----------------------------------------
-bc_set_top = findall(pc.position[2,:] .> length_y / 2 - 5.1 * Δx)
-bc_set_bottom = findall(pc.position[2,:] .< -length_y / 2 + 5.1 * Δx)
+bc_set_top = findall(pc.position[2, :] .> length_y / 2 - 5.1 * Δx)
+bc_set_bottom = findall(pc.position[2, :] .< -length_y / 2 + 5.1 * Δx)
 bc_top = VelocityBC(t -> 0.1, bc_set_top, 2)
 bc_bottom = VelocityBC(t -> -0.1, bc_set_bottom, 2)
 boundary_conditions = [bc_top, bc_bottom]
@@ -45,14 +51,14 @@ mkpath(resfolder)
 es = ExportSettings(resfolder, 10)
 
 ##--   7. DEFINE THE JOB   -----------------------------------------------------------------
-job = PDSingleBodyAnalysis(
-    name = simulation_name,
-    pc = pc,
-    mat = mat,
-    precracks = precracks,
-    bcs = boundary_conditions,
-    td = td,
-    es = es,
+job = PDSingleBodyAnalysis(;
+    name=simulation_name,
+    pc=pc,
+    mat=mat,
+    precracks=precracks,
+    bcs=boundary_conditions,
+    td=td,
+    es=es,
 )
 
 ##--   8. SUBMIT THE JOB / RUN THE ANALYSIS   ----------------------------------------------
