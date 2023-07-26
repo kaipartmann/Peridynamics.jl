be08ab0cc84d91bdc1188889a9d80437cde3caa4
Documenter.jl <documenter@juliadocs.github.io>
2023-07-26 18:19:38 +0000

    build based on afe5163

---
+++ v0.2.0/literate/howto_conditions.jl
@@ -0,0 +1,11 @@
+# # [Conditions](@id howto_conditions)
+# Short guide on how to define boundary and initial conditions.
+
+# ## Boundary Conditions
+# ### [`VelocityBC`](@ref)
+# Please refer to the [`VelocityBC`](@ref)
+# ### [`ForceDensityBC`](@ref)
+# ### [`PosDepVelBC`](@ref)
+
+# ## Initial Conditions
+# #### [`VelocityIC`](@ref)
