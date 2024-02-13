0611304d4e21e5da5d1b483d5447b4de4dfe2d16
Kai Partmann <kai.partmann@gmail.com>
2024-02-13 08:09:38 +0100

    Remove archive code

--- src/archive/material_interface.jl
+++
@@ -1,9 +0,0 @@
-"""
-    init_body(mat::PDMaterial, pc::PointCloud)
-
-Material interface function for initialization of the `body <: AbstractPDBody` used for
-the simulation.
-"""
-function init_body end
-
-function compute_forcedensity! end
