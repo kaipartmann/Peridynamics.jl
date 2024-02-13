2195c50595b0f2735e367362cf06cd0059bd2554
Kai Partmann <kai.partmann@gmail.com>
2024-02-13 14:47:43 +0100

    Implement CKIMaterial in new API

--- src/physics/continuum_kinematics_inspired.jl
+++ src/physics/continuum_kinematics_inspired.jl
@@ -1,42 +1,34 @@
-struct CKIMaterial <: AbstractMaterial
+struct CKIMaterial <: AbstractMaterial end
+
+struct CKIPointParameters <: AbstractPointParameters
     δ::Float64
     rho::Float64
     E::Float64
     nu::Float64
     G::Float64
     K::Float64
+    λ::Float64
+    μ::Float64
+    Gc::Float64
+    εc::Float64
     C1::Float64
     C2::Float64
     C3::Float64
-    Gc::Float64
-    εc::Float64
 end
 
-function CKIMaterial(; horizon::Real, rho::Real, E::Real, nu::Real,
-                     Gc::Real=-1, epsilon_c::Real=-1, C1::Real=0,
-                     C2::Real=0, C3::Real=0)
-    K = E / (3 * (1 - 2 * nu))
-    G = E / (2 * (1 + nu))
-    λ = E * nu / ((1 + nu) * (1 - 2nu))
-    μ = G
-    if (Gc !== -1) && (epsilon_c == -1)
-        epsilon_c = sqrt(5.0 * Gc / (9.0 * K * horizon))
-    elseif (Gc == -1) && (epsilon_c !== -1)
-        Gc = 9.0 / 5.0 * K * horizon * epsilon_c^2
-    elseif (Gc !== -1) && (epsilon_c !== -1)
-        msg = "duplicate definition: define either Gc or epsilon_c, not both!"
-        throw(ArgumentError(msg))
-    elseif (Gc == -1) && (epsilon_c == -1)
-        throw(ArgumentError("either Gc or epsilon_c have to be defined!"))
-    end
-    if C1 == 0 && C2 == 0 && C3 == 0
-        C1 = 30 / π * μ / horizon^4
-        C2 = 0.0
-        C3 = 32 / π^4 * (λ - μ) / horizon^12
-    else
-        msg = "CPD parameters choosen manually!\n"
-        msg *= "Be careful when adjusting CPD parameters to avoid unexpected outcomes!"
-        @warn msg
-    end
-    return CKIMaterial(horizon, rho, E, nu, G, K, C1, C2, C3, Gc, epsilon_c)
+@inline point_param_type(::CKIMaterial) = CKIPointParameters
+
+function get_point_params(::CKIMaterial, p::Dict{Symbol,Any})
+    δ = get_horizon(p)
+    rho = get_density(p)
+    E, nu, G, K, λ, μ = get_elastic_params(p)
+    Gc, εc = get_frac_params(p, δ, K)
+    C1 = 30 / π * μ / δ^4
+    C2 = 0.0
+    C3 = 32 / π^4 * (λ - μ) / δ^12
+    return CKIPointParameters(δ, rho, E, nu, G, K, λ, μ, Gc, εc, C1, C2, C3)
 end
+
+@inline allowed_material_kwargs(::CKIMaterial) = DEFAULT_POINT_KWARGS
+
+@inline default_export_fields(::Type{CKIMaterial}) = (:displacement, :damage)
