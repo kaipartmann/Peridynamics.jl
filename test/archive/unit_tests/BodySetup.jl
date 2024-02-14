301594256973585c6a806fb62150813e7659284f
Kai Partmann <kai.partmann@gmail.com>
2024-02-14 08:54:31 +0100

    Removed old tests

--- test/archive/unit_tests/BodySetup.jl
+++
@@ -1,6 +0,0 @@
-using Peridynamics, Test
-
-bs1 = BodySetup(PointCloud(1, 1, 1, 0.5), BBMaterial(; horizon=1, rho=1, E=1, Gc=1))
-@test isempty(bs1.precracks)
-@test isempty(bs1.bcs)
-@test isempty(bs1.ics)
--- test/archive/unit_tests/defaultdist.jl
+++
@@ -1,5 +0,0 @@
-using Peridynamics, Test
-
-@test Peridynamics.defaultdist(4, 2) == [1:2, 3:4]
-@test Peridynamics.defaultdist(3, 2) == [1:2, 3:3]
-@test Peridynamics.defaultdist(2, 3) == [1:1, 2:2, 0:-1]
