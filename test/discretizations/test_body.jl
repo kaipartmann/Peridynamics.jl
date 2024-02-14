a26bfdb451011493fb0f02110e6dfaeb0bbc6ca9
Kai Partmann <kai.partmann@gmail.com>
2024-02-14 14:07:31 +0100

    Improve clarity regarding which feature is tested

--- test/discretizations/test_body.jl
+++ test/discretizations/test_body.jl
@@ -13,7 +13,7 @@
     @test body.single_dim_bcs == Vector{Peridynamics.SingleDimBC}()
     @test body.single_dim_ics == Vector{Peridynamics.SingleDimIC}()
     @test body.point_sets_precracks == Vector{Peridynamics.PointSetsPreCrack}()
-    @test isempty(body.point_sets)
+    @test body.point_sets == Dict{Symbol,Vector{Int}}()
     @test body.point_params == Vector{Peridynamics.BBPointParameters}()
     @test body.params_mapping == zeros(Int, n_points)
 end
@@ -33,7 +33,7 @@ end
     @test body.single_dim_bcs == Vector{Peridynamics.SingleDimBC}()
     @test body.single_dim_ics == Vector{Peridynamics.SingleDimIC}()
     @test body.point_sets_precracks == Vector{Peridynamics.PointSetsPreCrack}()
-    @test isempty(body.point_sets)
+    @test body.point_sets == Dict{Symbol,Vector{Int}}()
     @test body.point_params == Vector{Peridynamics.BBPointParameters}()
     @test body.params_mapping == zeros(Int, n_points)
 end
@@ -47,18 +47,10 @@ end
     mat = BBMaterial()
     body = Body(mat, position, volume)
 
-    # test body creation
-    @test body.mat == BBMaterial()
+    # test body
     @test body.n_points == 4
     @test body.position == position
-    @test body.volume == volume
-    @test body.failure_allowed == BitVector(fill(true, 4))
-    @test body.single_dim_bcs == Vector{Peridynamics.SingleDimBC}()
-    @test body.single_dim_ics == Vector{Peridynamics.SingleDimIC}()
-    @test body.point_sets_precracks == Vector{Peridynamics.PointSetsPreCrack}()
-    @test isempty(body.point_sets)
-    @test body.point_params == Vector{Peridynamics.BBPointParameters}()
-    @test body.params_mapping == zeros(Int, 4)
+    @test body.point_sets == Dict{Symbol,Vector{Int}}()
 
     # add point set
     point_set!(body, :a, 1:2)
@@ -80,25 +72,16 @@ end
 
 @testitem "material!" begin
     # setup
-    position = [0.0 1.0 0.0 0.0
-                0.0 0.0 1.0 0.0
-                0.0 0.0 0.0 1.0]
-    volume = [1, 1, 1, 1]
-    mat = BBMaterial()
+    n_points = 4
+    mat, position, volume = BBMaterial(), rand(3, n_points), rand(n_points)
     body = Body(mat, position, volume)
 
     # test body creation
     @test body.mat == BBMaterial()
-    @test body.n_points == 4
-    @test body.position == position
-    @test body.volume == volume
-    @test body.failure_allowed == BitVector(fill(true, 4))
-    @test body.single_dim_bcs == Vector{Peridynamics.SingleDimBC}()
-    @test body.single_dim_ics == Vector{Peridynamics.SingleDimIC}()
-    @test body.point_sets_precracks == Vector{Peridynamics.PointSetsPreCrack}()
-    @test isempty(body.point_sets)
+    @test body.n_points == n_points
     @test body.point_params == Vector{Peridynamics.BBPointParameters}()
-    @test body.params_mapping == zeros(Int, 4)
+    @test body.params_mapping == zeros(Int, n_points)
+    @test isempty(body.point_sets)
 
     # add point set
     point_set!(body, :a, 1:2)
@@ -133,35 +116,20 @@ end
 
 @testitem "velocity_bc!" begin
     # setup
-    position = [0.0 1.0 0.0 0.0
-                0.0 0.0 1.0 0.0
-                0.0 0.0 0.0 1.0]
-    volume = [1, 1, 1, 1]
-    mat = BBMaterial()
+    n_points = 4
+    mat, position, volume = BBMaterial(), rand(3, n_points), rand(n_points)
     body = Body(mat, position, volume)
 
     # test body creation
-    @test body.n_points == 4
+    @test body.n_points == n_points
     @test body.position == position
     @test body.volume == volume
-    @test body.failure_allowed == BitVector(fill(true, 4))
+    @test isempty(body.point_sets)
 
     # add point set
     point_set!(body, :a, 1:2)
-    @test body.point_sets == Dict(:a => 1:2)
-
-    # add another point set via function definition
-    point_set!(x -> x > 0.5, body, :b)
-    @test body.point_sets == Dict(:a => 1:2, :b => [2])
-
-    # add point set with do syntax
-    point_set!(body, :c) do p
-        p[3] > 0.0
-    end
-    @test body.point_sets == Dict(:a => 1:2, :b => [2], :c => [4])
-
-    # point_set!
-    @test_throws BoundsError point_set!(body, :d, 1:5)
+    point_set!(body, :b, 3:4)
+    @test body.point_sets == Dict(:a => 1:2, :b => 3:4)
 
     # add material
     material!(body; horizon=1, E=1, rho=1, Gc=1)
@@ -240,20 +208,20 @@ end
 
 @testitem "forcedensity_bc!" begin
     # setup
-    position = [0.0 1.0 0.0 0.0
-                0.0 0.0 1.0 0.0
-                0.0 0.0 0.0 1.0]
-    volume = [1, 1, 1, 1]
-    mat = BBMaterial()
+    n_points = 4
+    mat, position, volume = BBMaterial(), rand(3, n_points), rand(n_points)
     body = Body(mat, position, volume)
+
+    # test body creation
+    @test body.n_points == n_points
+    @test body.position == position
+    @test body.volume == volume
+    @test isempty(body.point_sets)
 
     # add point set
     point_set!(body, :a, 1:2)
-    @test body.point_sets == Dict(:a => 1:2)
-
-    # add another point set via function definition
-    point_set!(x -> x > 0.5, body, :b)
-    @test body.point_sets == Dict(:a => 1:2, :b => [2])
+    point_set!(body, :b, 3:4)
+    @test body.point_sets == Dict(:a => 1:2, :b => 3:4)
 
     # velocity bc 1
     forcedensity_bc!(t -> 1, body, :a, 1)
