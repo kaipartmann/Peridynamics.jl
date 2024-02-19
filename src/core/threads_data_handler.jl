b50e47e7f3fff141fba8364b0eaf182d4cc3ea07
Kai Partmann <kai.partmann@gmail.com>
2024-02-19 15:27:36 +0100

    Each chunk has own portion of halo exchanges

--- src/core/threads_data_handler.jl
+++ src/core/threads_data_handler.jl
@@ -1,11 +1,15 @@
 struct ThreadsDataHandler{C<:AbstractBodyChunk}
     chunks::Vector{C}
-    halo_exchanges::Vector{HaloExchange}
+    halo_exchanges::Vector{Vector{HaloExchange}}
 end
 
 function ThreadsDataHandler(body::Body, time_solver::AbstractTimeSolver,
                             point_decomp::PointDecomposition)
     body_chunks = chop_body_threads(body, time_solver, point_decomp)
-    halo_exchanges = find_halo_exchanges(body_chunks)
+    _halo_exchanges = find_halo_exchanges(body_chunks)
+    halo_exchanges = [Vector{HaloExchange}() for _ in eachindex(body_chunks)]
+    @threads :static for chunk_id in eachindex(body_chunks)
+        halo_exchanges[chunk_id] = filter(x -> x.to_chunk_id == chunk_id, _halo_exchanges)
+    end
     return ThreadsDataHandler(body_chunks, halo_exchanges)
 end
