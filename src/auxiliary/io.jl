cb3b408344b4c67f54fd1de9b319ff8d1c23d9bb
Kai Partmann <kai.partmann@gmail.com>
2024-02-20 14:58:23 +0100

    Fix job keywords

--- src/auxiliary/io.jl
+++ src/auxiliary/io.jl
@@ -22,15 +22,17 @@ function get_export_options(::Type{M}, o::Dict{Symbol,Any}) where {M<:AbstractMa
     local freq::Int
     local fields::NTuple{N,Symbol} where {N}
 
-    if haskey(o, :path)
+    if haskey(o, :path) && haskey(o, :freq)
         root = string(o[:path])
+        freq = Int(o[:freq])
+    elseif haskey(o, :path) && !haskey(o, :freq)
+        root = string(o[:path])
+        freq = 10
+    elseif !haskey(o, :path) && haskey(o, :freq)
+        msg = "if `freq` is spedified, the keyword `path` is also needed!\n"
+        throw(ArgumentError(msg))
     else
         root = ""
-    end
-
-    if haskey(o, :freq)
-        freq = Int(o[:freq])
-    else
         freq = 0
     end
     freq < 0 && throw(ArgumentError("`freq` should be larger than zero!\n"))
