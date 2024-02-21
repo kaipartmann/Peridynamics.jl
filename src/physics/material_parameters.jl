a13fce561340c262b44327d12d741f8a55e92775
Kai Partmann <kai.partmann@gmail.com>
2024-02-21 11:57:49 +0100

    Remove unnecessary type assertions

--- src/physics/material_parameters.jl
+++ src/physics/material_parameters.jl
@@ -17,27 +17,19 @@ function get_density(p::Dict{Symbol,Any})
 end
 
 function get_elastic_params(p::Dict{Symbol,Any})
-    local E::Float64
-    local nu::Float64
-    local G::Float64
-    local K::Float64
-    local λ::Float64
-    local μ::Float64
-
     if !haskey(p, :E) || !haskey(p, :nu)
         msg = "insufficient keywords for calculation of elastic parameters!\n"
         msg *= "The keywords `E` (elastic modulus) and `nu` (poisson ratio) are needed!\n"
         throw(ArgumentError(msg))
     end
-    E = float(p[:E])
+    E::Float64 = float(p[:E])
     E ≤ 0 && throw(ArgumentError("`E` should be larger than zero!\n"))
-    nu = float(p[:nu])
+    nu::Float64 = float(p[:nu])
     nu ≤ 0 && throw(ArgumentError("`nu` should be larger than zero!\n"))
     nu ≥ 1 && throw(ArgumentError("too high value of `nu`! Condition: 0 < `nu` ≤ 1\n"))
     G = E / (2 * (1 + nu))
     K = E / (3 * (1 - 2 * nu))
     λ = E * nu / ((1 + nu) * (1 - 2nu))
     μ = G
-
     return E, nu, G, K, λ, μ
 end
