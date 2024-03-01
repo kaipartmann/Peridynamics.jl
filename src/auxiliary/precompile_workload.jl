3788cef15d2fe605991857475af53d0d8a6391b2
Kai Partmann <kai.partmann@gmail.com>
2024-03-01 12:38:49 +0100

    Body now inside of compile workload

--- src/auxiliary/precompile_workload.jl
+++ src/auxiliary/precompile_workload.jl
@@ -2,10 +2,12 @@
     l, Δx, δ, a = 1.0, 1 / 4, 3.015 / 4, 0.5
     root = joinpath(@__DIR__, "temp_precompilation")
     pos, vol = uniform_box(l, l, l, Δx)
-    b1 = Body(BBMaterial(), pos, vol)
-    b2 = Body(OSBMaterial(), pos, vol)
-    b3 = Body(NOSBMaterial(), pos, vol)
+    path_bb = joinpath(root, "BB")
+    path_osb = joinpath(root, "OSB")
+    path_nosb = joinpath(root, "NOSB")
+
     @compile_workload begin
+        b1 = Body(BBMaterial(), pos, vol)
         material!(b1; horizon=3.015Δx, E=2.1e5, rho=8e-6, Gc=2.7)
         point_set!(p -> p[1] ≤ -l / 2 + a && 0 ≤ p[2] ≤ 2δ, b1, :set_a)
         point_set!(p -> p[1] ≤ -l / 2 + a && -2δ ≤ p[2] < 0, b1, :set_b)
@@ -30,9 +32,8 @@
         forcedensity_bc!(t -> 0, b1, :set_bottom, 1)
         forcedensity_bc!(t -> 0, b1, :set_bottom, 2)
         forcedensity_bc!(t -> 0, b1, :set_bottom, 3)
-        submit(Job(b1, VelocityVerlet(steps=2); path=joinpath(root, "BB"), freq=1);
-               quiet=true)
 
+        b2 = Body(OSBMaterial(), pos, vol)
         material!(b2; horizon=3.015Δx, E=2.1e5, nu=0.25, rho=8e-6, Gc=2.7)
         point_set!(p -> p[1] ≤ -l / 2 + a && 0 ≤ p[2] ≤ 2δ, b2, :set_a)
         point_set!(p -> p[1] ≤ -l / 2 + a && -2δ ≤ p[2] < 0, b2, :set_b)
@@ -42,9 +43,8 @@
         point_set!(p -> p[2] < -l / 2 + Δx, b2, :set_bottom)
         velocity_bc!(t -> -30, b2, :set_bottom, :y)
         velocity_bc!(t -> 30, b2, :set_top, 2)
-        submit(Job(b2, VelocityVerlet(steps=2); path=joinpath(root, "OSB"), freq=1);
-               quiet=true)
 
+        b3 = Body(NOSBMaterial(), pos, vol)
         material!(b3; horizon=3.015Δx, E=2.1e5, nu=0.25, rho=8e-6, Gc=2.7)
         point_set!(p -> p[1] ≤ -l / 2 + a && 0 ≤ p[2] ≤ 2δ, b3, :set_a)
         point_set!(p -> p[1] ≤ -l / 2 + a && -2δ ≤ p[2] < 0, b3, :set_b)
@@ -54,8 +54,13 @@
         point_set!(p -> p[2] < -l / 2 + Δx, b3, :set_bottom)
         velocity_bc!(t -> -30, b3, :set_bottom, :y)
         velocity_bc!(t -> 30, b3, :set_top, 2)
-        submit(Job(b3, VelocityVerlet(steps=2); path=joinpath(root, "NOSB"), freq=1);
-               quiet=true)
+
+        vv = VelocityVerlet(steps=2)
+
+        submit(Job(b1, vv; path=path_bb, freq=1); quiet=true)
+        submit(Job(b2, vv; path=path_osb, freq=1); quiet=true)
+        submit(Job(b3, vv; path=path_nosb, freq=1); quiet=true)
     end
+
     rm(root; recursive=true, force=true)
 end
