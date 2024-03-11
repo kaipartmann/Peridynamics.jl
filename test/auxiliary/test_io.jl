53a9be3f3281bbe870e2714405e45153d50e6c33
Kai Partmann <kai.partmann@gmail.com>
2024-03-11 12:14:29 +0100

    Changed export of fields to use a function

--- test/auxiliary/test_io.jl
+++ test/auxiliary/test_io.jl
@@ -6,7 +6,7 @@
     @test eo.vtk == joinpath("rootpath", "vtk")
     @test eo.logfile == joinpath("rootpath", "logfile.log")
     @test eo.freq == 10
-    @test eo.fields == ((:displacement, Matrix{Float64}), (:damage, Vector{Float64}))
+    @test eo.eff == Peridynamics.export_disp_and_dmg
 
     o = Dict{Symbol,Any}(:path => "rootpath")
     eo = Peridynamics.get_export_options(Peridynamics.BBVerletStorage, o)
@@ -15,7 +15,7 @@
     @test eo.vtk == joinpath("rootpath", "vtk")
     @test eo.logfile == joinpath("rootpath", "logfile.log")
     @test eo.freq == 10
-    @test eo.fields == ((:displacement, Matrix{Float64}), (:damage, Vector{Float64}))
+    @test eo.eff == Peridynamics.export_disp_and_dmg
 
     o = Dict{Symbol,Any}(:freq => 10)
     msg = "if `freq` is spedified, the keyword `path` is also needed!\n"
@@ -30,7 +30,8 @@
     @test eo.vtk == ""
     @test eo.logfile == ""
     @test eo.freq == 0
-    @test eo.fields == NTuple{0,Tuple{Symbol,DataType}}()
+    @test eo.eff isa Function
+    @test eo.eff() == ()
 
     o = Dict{Symbol,Any}(:path => "rootpath", :freq => -10)
     msg = "`freq` should be larger than zero!\n"
@@ -38,18 +39,55 @@
         Peridynamics.get_export_options(Peridynamics.BBVerletStorage, o)
     end
 
-    o = Dict{Symbol,Any}(:path => "rootpath", :write => (:displacement, :damage, :b_int))
+    eff = x -> (("disp", x.displacement), ("dmg", x.damage), ("b_int", x.b_int))
+    o = Dict{Symbol,Any}(:path => "rootpath", :eff => eff)
     eo = Peridynamics.get_export_options(Peridynamics.BBVerletStorage, o)
     @test eo.exportflag == true
     @test eo.root == "rootpath"
     @test eo.vtk == joinpath("rootpath", "vtk")
     @test eo.logfile == joinpath("rootpath", "logfile.log")
     @test eo.freq == 10
-    @test eo.fields == ((:displacement, Matrix{Float64}),
-                        (:damage, Vector{Float64}),
-                        (:b_int, Matrix{Float64}))
+    @test eo.eff == eff
 end
 
-@testitem "export_results" begin
+@testitem "exported fields function" begin
+    position = rand(3,10)
+    displacement = rand(3,10)
+    velocity = rand(3,10)
+    velocity_half = rand(3,10)
+    acceleration = rand(3,10)
+    b_int = rand(3,10)
+    b_ext = rand(3,10)
+    damage = rand(10)
+    bond_active = rand(Bool,20)
+    n_active_bonds = rand(Int,20)
+    s = Peridynamics.BBVerletStorage(position, displacement, velocity, velocity_half,
+                                     acceleration, b_int, b_ext, damage, bond_active,
+                                     n_active_bonds)
 
+    o = Dict{Symbol,Any}(:path => "rootpath")
+    eo = Peridynamics.get_export_options(Peridynamics.BBVerletStorage, o)
+    tpl = eo(s)
+    @test length(tpl) == 2
+    @test tpl[1] == ("displacement", displacement)
+    @test tpl[2] == ("damage", damage)
+
+    _eff = x -> (("disp", x.displacement), ("dmg", x.damage), ("b_int", x.b_int))
+    o = Dict{Symbol,Any}(:path => "rootpath", :eff => _eff)
+    eo = Peridynamics.get_export_options(Peridynamics.BBVerletStorage, o)
+    tpl = eo(s)
+    @test length(tpl) == 3
+    @test tpl[1] == ("disp", displacement)
+    @test tpl[2] == ("dmg", damage)
+    @test tpl[3] == ("b_int", b_int)
+
+    _eff = @eff :displacement :acceleration :damage :b_int
+    o = Dict{Symbol,Any}(:path => "rootpath", :eff => _eff)
+    eo = Peridynamics.get_export_options(Peridynamics.BBVerletStorage, o)
+    tpl = eo(s)
+    @test length(tpl) == 4
+    @test tpl[1] == ("displacement", displacement)
+    @test tpl[2] == ("acceleration", acceleration)
+    @test tpl[3] == ("damage", damage)
+    @test tpl[4] == ("b_int", b_int)
 end
