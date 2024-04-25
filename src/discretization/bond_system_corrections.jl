e68c2ba0c20c65af8d16e2ec7fc3824af9388310
Kai Partmann <kai.partmann@gmail.com>
2024-04-25 11:51:51 +0200

    Add calculation of `mfactor`

--- src/discretization/bond_system_corrections.jl
+++ src/discretization/bond_system_corrections.jl
@@ -28,3 +28,43 @@ end
                                            bond_id::Int)
     return correction.scfactor[bond_id]
 end
+
+function initialize!(chunk::BodyChunk{BondSystem{EnergySurfaceCorrection}})
+    inititalize_mfactor!(chunk)
+    return nothing
+end
+
+function inititalize_mfactor!(chunk::BodyChunk{BondSystem{EnergySurfaceCorrection}})
+    system = chunk.system
+    mfactor = system.correction.mfactor
+    stendens = zeros(3, n_points)
+    for d in 1:3
+        defposition .= copy(system.position)
+        defposition[d,:] .*= 1.001
+        @views calc_stendens!(stendens[d,:], defposition, chunk)
+        for i in each_point_idx(chunk)
+            param = get_params(chunk, i)
+            mfactor[d,i] = 0.5 * param.G * 1e-6 / stendens[d,i]
+        end
+    end
+    return nothing
+end
+
+function calc_stendens!(stendens, defposition, chunk)
+    system = chunk.system
+    for i in each_point_idx(chunk)
+        param = get_params(chunk, i)
+        temp = 15 * param.G /(2π * param.δ * param.δ * param.δ * param.δ)
+        for bond_id in each_bond_idx(system, i)
+            bond = system.bonds[bond_id]
+            j, L = bond.neighbor, bond.length
+            Δxijx = defposition[1, j] - defposition[1, i]
+            Δxijy = defposition[2, j] - defposition[2, i]
+            Δxijz = defposition[3, j] - defposition[3, i]
+            l = sqrt(Δxijx * Δxijx + Δxijy * Δxijy + Δxijz * Δxijz)
+            ε = (l - L) / L
+            stendens[i] += temp * ε * ε * L * system.volume[j]
+        end
+    end
+    return nothing
+end
