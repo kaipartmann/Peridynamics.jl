1dcfea5320c512b0a653f5a4e167f0e8d6d21a1b
Kai Partmann <kai.partmann@gmail.com>
2024-05-16 11:28:06 +0200

    Initial commit

--- test/core/test_submit.jl
+++
@@ -1,9 +0,0 @@
-@testitem "get_submit_options" begin
-    o = Dict{Symbol,Any}(:quiet => true)
-    quiet = Peridynamics.get_submit_options(o)
-    @test quiet == true
-
-    o = Dict{Symbol,Any}()
-    quiet = Peridynamics.get_submit_options(o)
-    @test quiet == false
-end
