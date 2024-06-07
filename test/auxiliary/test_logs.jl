3c8bf5923318a2432578c956a409e1f800d9f21c
Kai Partmann <kai.partmann@gmail.com>
2024-06-07 09:29:42 +0200

    Added tests for logging

--- test/auxiliary/test_logs.jl
+++ test/auxiliary/test_logs.jl
@@ -12,9 +12,34 @@ end
     @test Peridynamics.mpi_run() == true
     enable_mpi_progress_bars!()
     @test Peridynamics.mpi_progress_bars() == true
-    @test Peridynamics.progress_bars() == false
     @test_logs (:warn,) (Peridynamics.set_progress_bars!())
     @test Peridynamics.progress_bars() == true
+    reset_mpi_progress_bars!()
+    @test Peridynamics.mpi_progress_bars() == false
+    Peridynamics.set_progress_bars!()
+    @test Peridynamics.progress_bars() == false
+end
+
+@testitem "log_create_data_handler" begin
+    # setup
+    Peridynamics.set_quiet!(false)
+    @test Peridynamics.quiet() == false
+    force_mpi_run!()
+    @test Peridynamics.mpi_run() == true
+    enable_mpi_progress_bars!()
+    @test Peridynamics.mpi_progress_bars() == true
+    @test_logs (:warn,) (Peridynamics.set_progress_bars!())
+    @test Peridynamics.progress_bars() == true
+
+    io = IOBuffer()
+    Peridynamics.log_create_data_handler_start(io)
+    msg_start = String(take!(io))
+    @test msg_start == "DATA HANDLER CREATION ... ⏳"
+    Peridynamics.log_create_data_handler_end(io)
+    msg_end = String(take!(io))
+    @test msg_end == "\rDATA HANDLER CREATION COMPLETED ✓\n"
+
+    # reset
     reset_mpi_progress_bars!()
     @test Peridynamics.mpi_progress_bars() == false
     Peridynamics.set_progress_bars!()
