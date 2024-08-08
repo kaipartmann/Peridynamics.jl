c9a99b21da1a82a222c31fc12e2ff6a766ce8987
Documenter.jl <documenter@juliadocs.github.io>
2024-08-08 23:07:13 +0000

    build based on 911f762

---
+++ v0.3.2/literate/tutorial_tension_precrack.jl
@@ -0,0 +1,118 @@
+# # [Tension with predefined crack](@id tutorial_tension_precrack)
+
+# Import the package:
+using Peridynamics
+# using GLMakie #src
+# using LaTeXStrings #src
+
+# First some geometrical parameters are defined.
+# These are edge length `l`, point spacing `Δx` and crack length `a`.
+# Now a cuboid body with the specified edge lengths and a thickness of one tenth thereof is
+# created using the bond-based material model.
+l, Δx, a = 1.0, 1/50, 0.5
+pos, vol = uniform_box(l, l, 0.1l, Δx)
+body = Body(BBMaterial(), pos, vol)
+
+#-
+# fig = Figure(resolution = (1000,500), figure_padding=0) #src
+# ax = Axis3(fig[1,1]; aspect = :data) #src
+# hidespines!(ax) #src
+# hidedecorations!(ax) #src
+# meshscatter!(ax, pc.position; markersize=0.5Δx, color=:grey80) #src
+# arrows!(ax, Point3[(0,-l/2,-0.08l),(-l/2,0,-0.08l),(-l/2*1.03,l/2*1.03,0)], #src
+#         Vec3[(0.1,0,0),(0,0.1,0),(0,0,0.05)], arrowsize=(0.03,0.03,0.05), #src
+#         linewidth=0.01, color=:black) #src
+# linesegments!(ax, Point3[(0,-l/2,-0.1l), (0,-l/2,-0.06l), (-l/2,0,-0.1l), #src
+#               (-l/2,0,-0.06l), (-l/2*1.03-0.02,l/2*1.03,0), #src
+#               (-l/2*1.03+0.02,l/2*1.03,0)]; linewidth=4, color=:black) #src
+# text!(ax, L"\mathbf{x}", position=Point3(0.18,-l/2,-0.08l), fontsize=30, #src
+#       align=(:center,:center), color=:black) #src
+# text!(ax, L"\mathbf{y}", position=Point3(-l/2,0.18,-0.08l), fontsize=30, #src
+#       align=(:center,:center), color=:black) #src
+# text!(ax, L"\mathbf{z}", position=Point3(-l/2*1.03,l/2*1.03,0.13), fontsize=30, #src
+#       align=(:center,:center), color=:black) #src
+# # save(joinpath(@__DIR__, "..", "assets", "tutorial_tension_1.png"), fig; px_per_unit=3) #src
+# fig #src
+
+#-
+# ![](https://github.com/kaipartmann/Peridynamics.jl/assets/68582683/ca3e724b-9dd7-417a-b3aa-bc37f81f4674) #md
+
+# The following material parameters are set:
+#
+# | material parameter | value |
+# |:--------|:-------------|
+# | Horizon $ δ $ | $3.015 \cdot Δx$ |
+# | Young's modulus $E$ | $ 210000 \, \mathrm{MPa}$ |
+# | Density $ρ$ | $ 8 \cdot 10^{-6}\,\mathrm{kg}\,\mathrm{mm}^{-3}$ |
+# | Griffith's parameter $G_c$ | $2.7 \, \mathrm{N} \, \mathrm{mm}^{-1}$ |
+δ = 3.015Δx
+material!(body; horizon=δ, E=2.1e5, rho=8e-6, Gc=2.7)
+
+# Two point sets are defined to insert a crack between them:
+point_set!(p -> p[1] ≤ -l/2+a && 0 ≤ p[2] ≤ 2δ, body, :set_a)
+point_set!(p -> p[1] ≤ -l/2+a && -2δ ≤ p[2] < 0, body, :set_b)
+precrack!(body, :set_a, :set_b)
+
+# fig = Figure(resolution = (1000,500), figure_padding=0) #src
+# ax = Axis3(fig[1,1]; aspect = :data) #src
+# hidespines!(ax) #src
+# hidedecorations!(ax) #src
+# meshscatter!(ax, pc.position; markersize=0.5Δx, color=:grey80) #src
+# arrows!(ax, Point3[(0,-l/2,-0.08l),(-l/2,0,-0.08l),(-l/2*1.03,l/2*1.03,0)], Vec3[(0.1,0,0),(0,0.1,0),(0,0,0.05)], arrowsize=(0.03,0.03,0.05), linewidth=0.01, color=:black) #src
+# linesegments!(ax, Point3[(0,-l/2,-0.1l), (0,-l/2,-0.06l), (-l/2,0,-0.1l), (-l/2,0,-0.06l), (-l/2*1.03-0.02,l/2*1.03,0), (-l/2*1.03+0.02,l/2*1.03,0)]; linewidth=4, color=:black) #src
+# text!(ax, L"\mathbf{x}", position=Point3(0.18,-l/2,-0.08l), fontsize=30, align=(:center,:center), color=:black) #src
+# text!(ax, L"\mathbf{y}", position=Point3(-l/2,0.18,-0.08l), fontsize=30, align=(:center,:center), color=:black) #src
+# text!(ax, L"\mathbf{z}", position=Point3(-l/2*1.03,l/2*1.03,0.13), fontsize=30, align=(:center,:center), color=:black) #src
+# meshscatter!(ax, pc.position[:, set_a]; markersize=0.5Δx, color=:blue) #src
+# meshscatter!(ax, pc.position[:, set_b]; markersize=0.5Δx, color=:red) #src
+# pca = scatter!(ax, [0,0,0]; markersize=30, color=:blue, label="set_a") #src
+# pcb = scatter!(ax, [0,0,0]; markersize=30, color=:red, label="set_b") #src
+# pca.visible[] = false #src
+# pcb.visible[] = false #src
+# axislegend(ax; framevisible=false, bgcolor=:transparent, labelsize=26) #src
+# save(joinpath(@__DIR__, "..", "assets", "tutorial_tension_2.png"), fig; px_per_unit=3) #src
+# fig #src
+# ![](https://github.com/kaipartmann/Peridynamics.jl/assets/68582683/36a16d3e-a2a5-4db7-9b73-55b321583902) #md
+
+# Two more point sets at the top and at the bottom are created, which are used for the
+# velocity boundary condition.
+point_set!(p -> p[2] > l/2-Δx, body, :set_top)
+point_set!(p -> p[2] < -l/2+Δx, body, :set_bottom)
+
+# fig = Figure(resolution = (1000,500), backgroundcolor = :transparent, figure_padding=0) #src
+# ax = Axis3(fig[1,1]; aspect = :data) #src
+# hidespines!(ax) #src
+# hidedecorations!(ax) #src
+# meshscatter!(ax, pc.position; markersize=0.5Δx, color=:grey80, transparency=false) #src
+# arrows!(ax, Point3[(0,-l/2,-0.08l),(-l/2,0,-0.08l),(-l/2*1.03,l/2*1.03,0)], Vec3[(0.1,0,0),(0,0.1,0),(0,0,0.05)], arrowsize=(0.03,0.03,0.05), linewidth=0.01, color=:black) #src
+# linesegments!(ax, Point3[(0,-l/2,-0.1l), (0,-l/2,-0.06l), (-l/2,0,-0.1l), (-l/2,0,-0.06l), (-l/2*1.03-0.02,l/2*1.03,0), (-l/2*1.03+0.02,l/2*1.03,0)]; linewidth=4, color=:black) #src
+# text!(ax, L"\mathbf{x}", position=Point3(0.18,-l/2,-0.08l), fontsize=30, align=(:center,:center), color=:black) #src
+# text!(ax, L"\mathbf{y}", position=Point3(-l/2,0.18,-0.08l), fontsize=30, align=(:center,:center), color=:black) #src
+# text!(ax, L"\mathbf{z}", position=Point3(-l/2*1.03,l/2*1.03,0.13), fontsize=30, align=(:center,:center), color=:black) #src
+# meshscatter!(ax, pc.position[:, set_top]; markersize=0.5Δx, color=:blue) #src
+# meshscatter!(ax, pc.position[:, set_bottom]; markersize=0.5Δx, color=:red) #src
+# pca = scatter!(ax, [0,0,0]; markersize=30, color=:blue, label="set_top") #src
+# pcb = scatter!(ax, [0,0,0]; markersize=30, color=:red, label="set_bottom") #src
+# pca.visible[] = false #src
+# pcb.visible[] = false #src
+# axislegend(ax; framevisible=false, bgcolor=:transparent, labelsize=26) #src
+# save(joinpath(@__DIR__, "..", "assets", "tutorial_tension_3.png"), fig; px_per_unit=3) #src
+# fig #src
+# ![](https://github.com/kaipartmann/Peridynamics.jl/assets/68582683/7c3e0b71-57b7-4bff-9bca-617bdae9f5be) #md
+
+# The tension is applied by moving the ends of the body apart at a constant speed of
+# $\pm 50 \, \mathrm{mm} \, \mathrm{s}^{-1}$:
+velocity_bc!(t -> -30, body, :set_bottom, :y)
+velocity_bc!(t -> 30, body, :set_top, :y)
+# The Velocity Verlet algorithm is used as time integration method and 2000 time steps
+# are calculated:
+vv = VelocityVerlet(steps=2000)
+# Now the job is defined
+job = Job(body, vv; path="results/mode_i_tension_precrack")
+# Finally the job is submitted to start simulations
+#md # ```julia
+#md # submit(job)
+#md # ```
+
+# ### Damage results:
+# ![](https://github.com/user-attachments/assets/918aed7b-735d-418f-900c-e0a996db2bab)
---
+++ v0.3.2/literate/tutorial_wave_in_bar.jl
@@ -0,0 +1,61 @@
+# # Wave propagation in a thin bar
+
+# In this tutorial, a cuboid bar is created.
+# A velocity pulse in the form of one period of a sine wave is applied to create
+# a pressure wave that propagates through the bar.
+#
+# First import the Peridynamics.jl package:
+using Peridynamics
+
+# To get started, some parameters used to for this simulation are defined.
+# These are the length of the bar `lx`, the width and height `lyz` and the number of
+# points in the width `npyz`.
+
+lx = 0.2
+lyz = 0.002
+npyz = 4
+
+# With these parameters the point spacing can be calculated:
+Δx = lyz / npyz
+# A cuboid body according to the ordinary state-based model with the specified dimensions
+# and point spacing is then created:
+pos, vol = uniform_box(lx, lyz, lyz, Δx)
+body = Body(OSBMaterial(), pos, vol)
+# Failure is prohibited throughout the body:
+failure_permit!(body, false)
+# Following material parameters are specified:
+
+# | material parameter | value |
+# |:--------|:-------------|
+# | Horizon $ δ $ | $3.015 \cdot Δx$ |
+# | Density $ρ$ | $ 7850\,\mathrm{kg}\,\mathrm{m}^{-3}$ |
+# | Young's modulus $E$ | $ 210 \, \mathrm{GPa}$ |
+# | Poisson's ratio $ν$ | $0.25$ |
+# | critical strain $ε_c$ | $0.01$ |
+material!(body, horizon=3.015Δx, rho=7850.0, E=210e9, nu=0.25, epsilon_c=0.01)
+# Point set `:left` including the first row of points in x-direction is created:
+point_set!(x -> x < -lx / 2 + 1.2Δx, body, :left)
+# The velocity boundary condition of the form
+# ```math
+#     {v}_x (t) =
+#     \begin{cases}
+#         v_\mathrm{max} \cdot \sin(2\pi \cdot \frac{t}{T}) \qquad
+#         & \forall \; 0 \leq t \leq T \\
+#         0 &\text{else}
+#     \end{cases}
+# ```
+# is applied to point set `:left`. The parameters used for this excitation are period
+# length `T` and amplitude `vmax`.
+T, vmax = 1.0e-5, 2.0
+velocity_bc!(t -> t < T ? vmax * sin(2π / T * t) : 0.0, body, :left, :x)
+
+# The Velocity Verlet algorithm is used as time integration method and 2000 time steps
+# are calculated:
+vv = VelocityVerlet(steps=2000)
+# The job is now defined with the specified settings and parameters.
+job = Job(body, vv; path="results/xwave")
+
+# The last step is submitting the job to start the simulation.
+#md # ```julia
+#md # submit(job);
+#md # ```
