737f8f31922e286c4d81b6333810c69dd54d629a
Manuel Dienst <manuel.dienst@student.uni-siegen.de>
2025-02-13 10:47:34 +0100

    Enable submitting 2 parameters that are not `nu` in bond-based, if they result in `nu=0.25`

--- test/physics/test_bond_based.jl
+++ test/physics/test_bond_based.jl
@@ -106,7 +106,18 @@ end
     @test Gc ≈ 1.0
     @test εc ≈ 0.6454972243679028
 
-    @test_throws ArgumentError material!(body, horizon=2, rho=1, E=1, G=0.4, Gc=1)
+    material!(body, horizon=2, rho=1, E=1, G=0.4, Gc=1)
+    (; δ, rho, E, nu, G, K, λ, μ, Gc, εc) = body.point_params[1]
+    @test δ ≈ 2.0
+    @test rho ≈ 1.0
+    @test E ≈ 1.0
+    @test nu ≈ 0.25
+    @test G ≈ 0.4
+    @test K ≈ 0.6666666666666666
+    @test λ ≈ 0.4
+    @test μ ≈ 0.4
+    @test Gc ≈ 1.0
+    @test εc ≈ 0.6454972243679028
 
     material!(body, horizon=2, rho=1, G=0.4, nu=0.25, Gc=1)
     (; δ, rho, E, nu, G, K, λ, μ, Gc, εc) = body.point_params[1]
@@ -121,4 +132,16 @@ end
     @test Gc ≈ 1.0
     @test εc ≈ 0.6454972243679028
 
+    material!(body, horizon=2, rho=1, lambda=0.4, mu=0.4, Gc=1)
+     (; δ, rho, E, nu, G, K, λ, μ, Gc, εc) = body.point_params[1]
+     @test δ ≈ 2.0
+     @test rho ≈ 1.0
+     @test E ≈ 1.0
+     @test nu ≈ 0.25
+     @test G ≈ 0.4
+     @test K ≈ 0.6666666666666666
+     @test λ ≈ 0.4
+     @test μ ≈ 0.4
+     @test Gc ≈ 1.0
+     @test εc ≈ 0.6454972243679028
 end
