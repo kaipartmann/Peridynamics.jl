1129009526d8b9092c78bf99d612ed7bcd1f15fe
Kai Partmann <kai.partmann@gmail.com>
2025-11-15 10:45:58 +0100

    Added tests of all the expected error handling

--- test/auxiliary/test_errors.jl
+++ test/auxiliary/test_errors.jl
@@ -1,9 +1,14 @@
 @testitem "InterfaceError" begin
+    using Peridynamics.Printf
     ie1 = Peridynamics.InterfaceError(Float64, println)
-    io = IOBuffer()
-    Base.showerror(IOContext(io, :compact=>false), ie1)
-    msg = String(take!(io))
     msg_correct = "interface method not correctly defined!"
     msg_correct *= "\n  type:    Float64\n  method:  println\n"
-    @test msg == msg_correct
+    @test sprint(showerror, ie1) == msg_correct
+end
+
+@testitem "NaNError" begin
+    using Peridynamics.Printf
+    ne1 = Peridynamics.NaNError(2.0, 2)
+    msg_correct = "NaN values detected in simulation data!\n  time:    2.0\n  step:    2\n"
+    @test sprint(showerror, ne1) == msg_correct
 end
